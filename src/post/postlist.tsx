08d0ecb222c77fabb6c3f9a3d6c5cc346920be81
Horn <2012847@cognizant.com>
2021-04-27 13:29:04 -0700

    added babel and jest

---
+++ src/post/postlist.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+// import {useSelector} from 'react-redux';
+
+const PostList = (/*props:PostProps*/) => {
+    return (
+        <h1 data-testid = "testone">Hello World</h1>
+    )
+}
+
+export default PostList;
+
