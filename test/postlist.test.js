65e7eb9fddb47a7a01877666dd4371cee46b85c8
Horn <2012847@cognizant.com>
2021-04-27 13:45:43 -0700

    changes

--- test/postlist.test.js
+++ test/postlist.test.js
@@ -6,20 +6,8 @@ import '@testing-library/jest-dom/extend-expect';
 afterEach(cleanup);
 
 describe("Tests for postlist, which is a list inclusion of all posts on the page.", () => {
-    it('Basic Test', () => {
+    test('Basic Test', () => {
         const {getByTestId} = render(<PostList/>);
         expect(getByTestId('testone')).toHaveTextContent("Hello World");
-    }),
-    
-    it('List does render', () => {
-
-    }),
-    
-    it('List should render 5 items by default.', () => {
-
-    }),
-
-    it('List should render 5 additional items when scrolling', () => {
-
     })
 })
