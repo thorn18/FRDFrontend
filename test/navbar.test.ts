a9b85cb1ecb2cf4f46fc4bceaa3eddab0ae964c0
Tater <2012849@cognizant.com>
2021-04-27 17:51:48 -0400

    added react types

--- test/navbar.test.ts
+++ test/navbar.test.ts
@@ -1,2 +1,7 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, cleanup} from '@testing-library/react'
+
+describe('test login menu', () => {
+    afterEach(cleanup)
+})
+
