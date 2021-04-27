f3b063a728f349e13927f7398ff8abd7502ba90d
Horn <2012847@cognizant.com>
2021-04-27 12:33:40 -0700

    changes to postlist test

---
+++ test/postlist.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {render, cleanup, fireEvent} from '@testing-libarary/react';
+//import TestEvent from './/';
+import '@testing-library/jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe("Tests for postlist", () => {
+    it('Should be a thing.', () => {
+        
+    })
+
+
+
+})
