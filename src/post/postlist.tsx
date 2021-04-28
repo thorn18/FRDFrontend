0958f93f18357df5c5c53cc55ae3fe8e62a3529c
Horn <2012847@cognizant.com>
2021-04-28 13:23:57 -0700

    moved hooks inside function

--- src/post/postlist.tsx
+++ src/post/postlist.tsx
@@ -4,19 +4,20 @@ import PostComponent from './PostComponent'
 import Post from '../models/post'
 import initialPostsState from '../store/reducer'
 import PostService from './post.service'
-import {AppState} from '../store/reducer'
+import { AppState } from '../store/reducer'
 
-let post_state: Post[] = useSelector((state: AppState) => state.postsState.posts);
-let [postStateLocal, setPostStateLocal] = useState(post_state);
-
-const dispatch = useDispatch();
-const getPosts = () => PostService.getAllPosts();
-
-useEffect(() => {
-    getPosts();
-}, [])
 
 const PostList = (props: Post[]) => {
+
+    let post_state: Post[] = useSelector((state: AppState) => state.postsState.posts);
+    let [postStateLocal, setPostStateLocal] = useState(post_state);
+
+    const dispatch = useDispatch();
+    const getPosts = () => PostService.getAllPosts();
+
+    useEffect(() => {
+        getPosts();
+    }, [])
 
     /**
      * This function will populate the post list component for the page.
