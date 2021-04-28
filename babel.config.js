7f869f3c757f523ab649603e35033e8f6f349c73
Kirsten Wood <2012853@cognizant.com>
2021-04-28 16:33:07 -0400

    added dependencies that were needed to get npm run sstart and npm run build to work

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-module.exports = function(api) {
-    api.cache(true);
-    return {
-        presets: [['@babel/preset-env', {targets: {node: 'current'}}], 
-        '@babel/preset-typescript',
-        '@babel/preset-react'
-    
-    ]
-    }
-}
-=======
 module.exports = {
 
     presets: [
@@ -23,4 +11,3 @@ module.exports = {
     ],
   
   };
->>>>>>> c8dd3308f9d21d64f2c3a499f6a5dfdbe2b6683e
