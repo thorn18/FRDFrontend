408a6f969707f09f6e377f3b3ab64d6d860d6159
Horn <2012847@cognizant.com>
2021-04-28 09:32:09 -0700

    postlist modifications to use mapping

--- src/post/postlist.tsx
+++ src/post/postlist.tsx
@@ -2,37 +2,19 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import PostComponent from '../post/post'
 
-const PostList = (/*props:PostProps*/) => {
+let post_state:Post[] = useSelector((state:PostState) => state.postReducer.posts);
+let [postStateLocal,setPostStateLocal] = useState(post_state);
 
-    let post_state:Post[] = useSelector((state:PostState) => state.postReducer.posts);
-    let [postlist,setpostlist] = useState(post_state);
-
+const PostList = (props:Post[]) => {
 
     /**
      * This function will populate the post list component for the page.
      */
-    function PopulateInitialPostList() {
-        // postlist.forEach((post) => {
-        //     let new_post = document.createElement('Post');
-        //     new_post.className = "post_element";
-            
-        // })
-    };
 
     return (
-        <>
-            <script type = 'text/javascript'>
-                postlist.forEach((post) => {
-
-                })
-            </script>
-            <ul className = "postlist" data-testid = "postlist" id = "postlistmain">
-                
-            </ul>
-            <script type = 'text/javascript'>
-                PopulatePostList();
-            </script>
-        </>
+        <div id = 'postListMain' data-testID = 'postListMain'>
+            {postStateLocal.map((post:Post) => <Post key={post.id} />)}
+        </div>
     )
 }
 export default PostList;
