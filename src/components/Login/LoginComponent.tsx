d1161a35be2e116577266868ab6f4bfaa77786b5
Horn <2012847@cognizant.com>
2021-05-03 15:00:23 -0700

    login form test pass

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -3,23 +3,20 @@ import User from '../../models/user';
 import pixelgramlogo from '../../pixelgram-logo.png'
 import './LoginComponent.css'
 
-interface userProps {
-    user: User;
-}
 
-function LoginComponent(props: userProps) {
+function LoginComponent() {
 
 
     return (
-        <div id="loginForm">
+        <div id="loginForm" data-testid = "loginForm">
             <div className = "loginInnerDiv">
                 <img src={pixelgramlogo} id="pixelImage"></img>
                 <form>
                     <input className="inputBox" id="username" type="text" aria-label="Username: " placeholder="Username"></input>
                     <input className="inputBox" id="password" type="password" aria-label="Password: " placeholder="Password"></input>
                     <div id="actionButtonContainer">
-                        <button className="register-button">Register</button>
-                        <button className="login-button">Login</button>
+                        <button className="register-button" data-testid = "registerbutton">Register</button>
+                        <button className="login-button" data-testid = "loginbutton">Login</button>
                     </div>
                 </form>
             </div>
--- test/login.test.tsx
+++ test/login.test.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
-import Navbar from "../src/components/Navbar/Navbar";
+import LoginComponent from  '../src/components/Login/LoginComponent'
 import "@testing-library/jest-dom/extend-expect";
 
 afterEach(cleanup);
 
+describe('Tests for the Login Component', () => {
+    it('Test to make sure that Login Button is visible', () => {
+        const { getByTestId } = render(< LoginComponent/>);
+        expect(getByTestId('loginForm')).toBeVisible();
+    })
+})
