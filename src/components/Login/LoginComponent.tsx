6093b72ce04f9eeb421c27fdc95c1ebd76b3718d
Nielsen <2012791@cognizant.com>
2021-05-04 17:02:54 -0500

    validation form control state and tests

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -1,22 +1,77 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import User from '../../models/user';
 import pixelgramlogo from '../../pixelgram-logo.png'
 import './LoginComponent.css'
 
+export interface Input {
+    username: any;
+    password: any;
+}
 
 function LoginComponent() {
+    const [input, setInput] = useState<Input>({ username: '', password: '' })
+    const [hasUsername, setUsername] = useState<boolean>(false);
+    const [hasPassword, setPassword] = useState<boolean>(false);
 
+    const handleInput = (e: SyntheticEvent) => {
+        let newInput = { ...input };
 
+        if ((e.target as HTMLInputElement).name === "username") {
+            newInput.username = (e.target as HTMLInputElement).value;
+            setInput(newInput);
+        } else if ((e.target as HTMLInputElement).name === "password") {
+            newInput.password = (e.target as HTMLInputElement).value;
+            setInput(newInput);
+        }
+
+        if (newInput.username !== '' && newInput.password !== '') {
+            setUsername(true);
+            setPassword(true);
+        }
+
+        if(newInput.username === ''){
+            setUsername(false);
+        }
+
+        if(newInput.password === ''){
+            setPassword(false);
+        }
+
+        console.log(hasUsername)
+        console.log({hasPassword})
+    }
     return (
-        <div id="loginForm" data-testid = "loginForm">
-            <div className = "loginInnerDiv">
+        <div id="loginForm" data-testid="loginForm">
+            <div className="loginInnerDiv">
                 <img src={pixelgramlogo} id="pixelImage"></img>
                 <form>
-                    <input className="inputBox" id="username" type="text" aria-label="Username: " placeholder="Username"></input>
-                    <input className="inputBox" id="password" type="password" aria-label="Password: " placeholder="Password"></input>
+                    <input
+                        className="inputBox"
+                        id="username"
+                        data-testid="username"
+                        name="username"
+                        type="text"
+                        aria-label="Username: "
+                        placeholder="Username"
+                        value={input.username}
+                        onChange={handleInput} />
+                    <input
+                        className="inputBox"
+                        id="password"
+                        data-testid="password"
+                        name="password"
+                        type="password"
+                        aria-label="Password: "
+                        placeholder="Password"
+                        value={input.password}
+                        onChange={handleInput} />
                     <div id="actionButtonContainer">
-                        <button className="register-button" data-testid = "registerbutton">Register</button>
-                        <button className="login-button" data-testid = "loginbutton">Login</button>
+                        <button className="register-button" data-testid="registerbutton">Register</button>
+                        <button
+                            className="login-button"
+                            disabled={hasUsername && hasPassword ? false : true}
+                            type="submit"
+                            data-testid="loginbutton">Login</button>
                     </div>
                 </form>
             </div>
--- test/login.test.tsx
+++ test/login.test.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
-import LoginComponent from  '../src/components/Login/LoginComponent'
+import LoginComponent, {Input} from  '../src/components/Login/LoginComponent'
 import "@testing-library/jest-dom/extend-expect";
+
+let setInput: (input: Input) => void;
+
+
+let input: Input = {username: '', password: ''};
+
+jest.mock('react', ()=> ({
+    ...jest.requireActual('react'),
+    useState: jest.fn()
+}))
+
+beforeEach(() => {
+    //jest.requireActual('react');
+
+    input = {username: '', password: ''};
+
+    setInput = jest.fn();
+
+ (useState as jest.Mock).mockImplementation(() => [input, setInput]);
+})
 
 afterEach(cleanup);
 
@@ -10,4 +30,32 @@ describe('Tests for the Login Component', () => {
         const { getByTestId } = render(< LoginComponent/>);
         expect(getByTestId('loginForm')).toBeVisible();
     })
+    it('Should call useState with default value', () => {
+        render(<LoginComponent/>);
+        expect(useState).toHaveBeenCalledWith(input);
+    })
+
+    it('Username should change if event fires', () => {
+        const {getByTestId} = render(<LoginComponent/>);
+        let username = getByTestId('username');
+        const value = 'test'
+        const testInput: Input = {username: value, password: ''}
+        fireEvent.change(username, {target: {value: value}});
+        expect(setInput).toHaveBeenCalledWith(testInput)
+    })
+
+    it('Password should change if event fires', () => {
+        const {getByTestId} = render(<LoginComponent/>);
+        let password = getByTestId('password');
+        const value = 'test'
+        const testInput: Input = {username: '', password: value}
+        fireEvent.change(password, {target: {value: value}});
+        expect(setInput).toHaveBeenCalledWith(testInput)
+    })
+
+    it('Login button should be disabled if no input', () => {
+        const {getByTestId} = render(<LoginComponent/>);
+        expect(getByTestId('loginbutton')).toBeDisabled();
+    })
+
 })
