93eca42943b1dc36007e6372316ee6f7884833d5
VU NGUYEN <vunguyen@VUs-MacBook-Pro-2.local>
2021-05-04 14:35:21 -0400

    add react-hook-form and retrive value from form input

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import User from '../../models/user';
 import pixelgramlogo from '../../pixelgram-logo.png'
-import './LoginComponent.css'
+import './LoginComponent.css';
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type FormValues = {
+    username: string;
+    password: string;
+};
 
 function LoginComponent() {
 
+    const { register, handleSubmit } = useForm<FormValues>();
+    const onSubmit: SubmitHandler<FormValues> = formData => {
+        // Get value from form input
+        console.log(formData);
+    }
 
     return (
         <div id="loginForm" data-testid = "loginForm">
             <div className = "loginInnerDiv">
                 <img src={pixelgramlogo} id="pixelImage"></img>
-                <form>
-                    <input className="inputBox" id="username" type="text" aria-label="Username: " placeholder="Username"></input>
-                    <input className="inputBox" id="password" type="password" aria-label="Password: " placeholder="Password"></input>
+                <form onSubmit={handleSubmit(onSubmit)}> 
+                    <input  {...register("username")} className="inputBox" id="username" type="text" aria-label="Username: " placeholder="Username"/>
+                    <input {...register("password")} className="inputBox" id="password" type="password" aria-label="Password: " placeholder="Password"/>
                     <div id="actionButtonContainer">
                         <button className="register-button" data-testid = "registerbutton">Register</button>
-                        <button className="login-button" data-testid = "loginbutton">Login</button>
+                        <input type="submit" className="login-button" data-testid = "loginbutton" value="Login"/>
                     </div>
                 </form>
             </div>
