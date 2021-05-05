8bf1928ed235dd3c22a12eb2bd34fde61ab3379e
Robert.Thompson <robert.thompson@cognizant.com>
2021-05-05 20:51:26 +0000

    Feature/navbar

--- test/Navbar.test.tsx
+++ test/Navbar.test.tsx
@@ -1,22 +1,159 @@
 import React from "react";
+import * as redux from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import Navbar from "../src/components/Navbar/Navbar";
 import "@testing-library/jest-dom/extend-expect";
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([]);
+
+let store;
 
 afterEach(cleanup);
 
-describe("test login menu", () => {
-  it("renders with menu closed", () => {
-    const { getAllByRole } = render(<Navbar />);
+describe('unathenticated user tests', () => {
+
+  beforeEach(() => {
+    store = mockStore({
+      userState: {
+        token: '',
+        loggenIn: false
+      }
+    });
+  });
+
+  it('displays logos', () => {
+    const { getByTestId } = render(<Provider store={store}><Navbar /></Provider>);
+
+    expect(getByTestId('nav-logo')).toBeVisible();
+  });
+
+  it('displays search bar', () => {
+    const { getByTestId } = render(<Provider store={store}><Navbar /></Provider>);
+
+    expect(getByTestId('search-bar')).toBeVisible();
+  });
+
+  it('renders with menu closed and no post button', () => {
+    const { getAllByRole } = render(<Provider store={store}><Navbar /></Provider>);
 
     expect(getAllByRole('button')).toHaveLength(1);
   });
-  
-  it("renders the menu button when open", async () => {
-    const { getByTestId } = render(<Navbar />);
+
+  it('displays login button', () => {
+    const { getByTestId } = render(<Provider store={store}><Navbar /></Provider>);
 
     fireEvent.click(getByTestId('toggle-btn'));
 
     expect(getByTestId('login-link')).toBeVisible();
-  })
-})
+  });
+
+  it('Navigates to login screen when clicking on login', () => {
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Navbar/>
+        </Router>
+      </Provider>
+    );
+
+    fireEvent.click(getByTestId('toggle-btn'));
+    fireEvent.click(getByTestId('login-link'));
+
+    expect(history.location.pathname).toBe('/login');
+  });
+});
+
+describe('authenticated user tests', () => {
+
+  beforeEach(() => {
+    store = mockStore({
+      userState: {
+        token: 'a',
+        loggedIn: true
+      }
+    });
+  });
+
+  it('displays logos', () => {
+    const { getByTestId } = render(<Provider store={store}><Navbar /></Provider>);
+
+    expect(getByTestId('nav-logo')).toBeVisible();
+  });
+
+  it('displays search bar', () => {
+    const { getByTestId } = render(<Provider store={store}><Navbar /></Provider>);
+  
+    expect(getByTestId('search-bar')).toBeVisible();
+  });
+
+  it("renders with menu closed and post button", () => {
+    const { getAllByRole } = render(<Provider store={store}><Navbar /></Provider>);
+  
+    expect(getAllByRole('button')).toHaveLength(2);
+  });
+  
+  it("displays logout button", () => {  
+    const { getByTestId } = render(<Provider store={store}><Navbar /></Provider>);
+  
+    fireEvent.click(getByTestId('toggle-btn'));
+  
+    expect(getByTestId('logout-link')).toBeVisible();
+  });
+
+  it('Navigates to the landing page when clicking on logout', () => {
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Navbar/>
+        </Router>
+      </Provider>
+    );
+
+    fireEvent.click(getByTestId('toggle-btn'));
+    fireEvent.click(getByTestId('logout-link'));
+
+    expect(history.location.pathname).toBe('/home');
+  });
+
+  it('Clears local storage upon logging out', () => {
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Navbar/>
+        </Router>
+      </Provider>
+    );
+
+    fireEvent.click(getByTestId('toggle-btn'));
+    fireEvent.click(getByTestId('logout-link'));
+
+    expect(localStorage.length).toEqual(0);
+  });
+
+  it('Calls dispatch to clear user when logging out', () => {
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Navbar/>
+        </Router>
+      </Provider>
+    );
+    
+    const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+    const mockedDispatch = jest.fn();
+    useDispatchSpy.mockReturnValue(mockedDispatch);
+
+    fireEvent.click(getByTestId('toggle-btn'));
+    fireEvent.click(getByTestId('logout-link'));
+
+    expect(mockedDispatch).toHaveBeenCalled();
+  });
+});
