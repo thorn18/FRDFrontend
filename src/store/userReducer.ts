e5c29735cbe25ba689b6ec14a4ba1d07aa7cd67d
Jamie.Tater <jamie.tater@cognizant.com>
2021-05-07 16:30:54 +0000

    Fix/errors

--- src/store/userReducer.ts
+++ src/store/userReducer.ts
@@ -2,24 +2,26 @@ import {userActionTypes} from './actions';
 
 export interface UserState {
     token: string,
-    loggedIn: boolean
+    loggedIn: boolean,
+    error: any
 }
 
 //initial states
 export const initialUserState: UserState = {
     token: '',
-    loggedIn: false
+    loggedIn: false,
+    error: undefined
 }
 
 //reducers
 const userReducer = (state: UserState = initialUserState, action: any) => {
     switch(action.type) {
         case userActionTypes.loginSuccess:
-            return {token: action.payload.token, loggedIn: true}
+            return {token: action.payload.token, loggedIn: true, error: undefined}
         case userActionTypes.loginError:
             return {token: state.token, error: action.payload, loggedIn: false}
         case userActionTypes.logout:
-            return {token: '', loggedIn: false};
+            return {token: '', loggedIn: false, error: undefined};
         default:
             return state;
     }
--- test/LoginComponent.test.tsx
+++ test/LoginComponent.test.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
-import LoginComponent from '../src/components/Login/LoginComponent'
+import LoginComponent, { Input } from '../src/components/Login/LoginComponent'
 import "@testing-library/jest-dom/extend-expect";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { useForm } from 'react-hook-form';
@@ -13,6 +13,10 @@ import { createMemoryHistory } from 'history';
 jest.mock('react-hook-form', () => ({
     ...jest.requireActual('react-hook-form'),
     useForm: jest.fn(),
+}));
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useState: jest.fn()
 }));
 jest.mock('react-redux', () => ({
     ...jest.requireActual('react-redux'),
@@ -35,23 +39,52 @@ describe('Tests for the Login Component', () => {
     let handleSubmit = jest.fn();
     let register = jest.fn();
 
+    //test variables
+    //declared here for scope reasons
+    let input: Input = { username: '', password: '' };
+    let hasUsername: boolean = false;
+    let hasPassword: boolean = false;
+    let userInteracted: boolean = false;
+    let passInteracted: boolean = false;
+
+    const setupWithSetState = () => {
+        (useState as jest.Mock).mockImplementationOnce(() => [input, jest.fn().mockImplementation((x) => {
+            let newinput = { ...input };
+            if (x.username) {
+                newinput.username = x.username;
+            } else if (x.password) {
+                newinput.password = x.password;
+            }
+            input = newinput;
+        })])
+            .mockImplementationOnce(() => [hasUsername, jest.fn().mockImplementation((x) => hasUsername = x)])
+            .mockImplementationOnce(() => [hasPassword, jest.fn().mockImplementation((x) => hasPassword = x)])
+            .mockImplementationOnce(() => [userInteracted, jest.fn().mockImplementation((x) => userInteracted = x)])
+            .mockImplementationOnce(() => [passInteracted, jest.fn().mockImplementation((x) => passInteracted = x)]);
+    }
+
     beforeEach(() => {
+        jest.clearAllMocks();
+
         handleSubmit = jest.fn();
         register = jest.fn();
         (useForm as jest.Mock).mockImplementation(() => { return { register, handleSubmit } });
     });
 
     it('Test to make sure that Login Form is visible', () => {
+        setupWithSetState();
         const { getByTestId } = render(<Provider store={store}> <LoginComponent /> </Provider>);
         expect(getByTestId('loginForm')).toBeVisible();
     });
 
     it('Test to make sure that Login Button is visible', () => {
+        setupWithSetState();
         const { getByTestId } = render(<Provider store={store}> <LoginComponent /> </Provider>);
         expect(getByTestId('loginbutton')).toBeVisible();
     });
 
     it('Test that clicking on loginbutton, the form submits', () => {
+        setupWithSetState();
         handleSubmit.mockImplementationOnce((x) => { return null });
         const { getByTestId } = render(<Provider store={store}> <LoginComponent /> </Provider>);
         expect(getByTestId('loginbutton')).toBeVisible();
@@ -61,7 +94,7 @@ describe('Tests for the Login Component', () => {
 
     it('Test that onSubmit dispatches to UserService', async () => {
 
-        const dispatch = jest.fn();
+        let dispatch = jest.fn();
         let token = '';
         dispatch.mockImplementation((x): void => {
             token = 'testToken';
@@ -73,13 +106,16 @@ describe('Tests for the Login Component', () => {
         (useSelector as jest.Mock).mockImplementation((x) => {
             return token;
         });
+
+        setupWithSetState();
+
         UserService.login = jest.fn().mockResolvedValue(200);
         let testFormData = {
             username: 'testUser',
             password: 'testPassword'
         }
         // x is onSubmit
-        handleSubmit.mockImplementation((x) => { x(testFormData); });
+        handleSubmit.mockImplementation((x) => { x() });
         const history = createMemoryHistory();
         const { getByTestId, rerender } = render(
             <Provider store={store}>
@@ -89,12 +125,55 @@ describe('Tests for the Login Component', () => {
             </Provider>
         );
         expect(getByTestId('loginbutton')).toBeVisible();
+
+        let username = getByTestId('username');
+        fireEvent.change(username, { target: { value: testFormData.username } });
+
+        let password = getByTestId('password');
+        fireEvent.change(password, { target: { value: testFormData.password } });
+        expect(input).toEqual(testFormData);
+
+        //RESET MOCKS
+        dispatch = jest.fn().mockImplementation((x): void => {
+            token = 'testToken';
+            return;
+        });
+        (useDispatch as jest.Mock).mockImplementation(() => {
+            return dispatch;
+        });
+        (useSelector as jest.Mock).mockImplementation((x) => {
+            return token;
+        });
+        setupWithSetState();
+        UserService.login = jest.fn().mockResolvedValue(200);
+        handleSubmit = jest.fn().mockImplementation((x) => { x() });
+        register = jest.fn();
+        (useForm as jest.Mock).mockImplementation(() => { return { register, handleSubmit } });
+        rerender(<LoginComponent />);
+
         fireEvent.click(getByTestId('loginbutton'));
-        await expect(handleSubmit).toHaveBeenCalledTimes(1);
+        expect(handleSubmit).toHaveBeenCalledTimes(1);
         expect(UserService.login).toHaveBeenCalledTimes(1);
         expect(UserService.login).toHaveBeenCalledWith(testFormData.username, testFormData.password);
         expect(dispatch).toHaveBeenCalledTimes(1);
+
+        //RESET MOCKS
+        dispatch = jest.fn().mockImplementation((x): void => {
+            token = 'testToken';
+            return;
+        });
+        (useDispatch as jest.Mock).mockImplementation(() => {
+            return dispatch;
+        });
+        (useSelector as jest.Mock).mockImplementation((x) => {
+            return token;
+        });
+        setupWithSetState();
+        UserService.login = jest.fn().mockResolvedValue(200);
+        handleSubmit = jest.fn().mockImplementation((x) => { x() });
+        register = jest.fn();
+        (useForm as jest.Mock).mockImplementation(() => { return { register, handleSubmit } });
         rerender(<LoginComponent />);
         expect(mockPush).toHaveBeenCalledWith('/home');
     });
-})
+});
