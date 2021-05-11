ae0a7c814d1938a397b0e323582cbb2f57ba5917
Nielsen <2012791@cognizant.com>
2021-05-11 11:56:43 -0500

    added threeDots css class

--- src/components/Post/PostComponent.tsx
+++ src/components/Post/PostComponent.tsx
@@ -33,7 +33,7 @@ function PostComponent(props: postProp) {
                 {post.user.profileImage ? <img className="pfp" src={post.user.profileImage} />
                     : <img className="pfp" src={'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'} />}
                 <span className="headerText">{post.user.username}</span>
-                <button className="deletePostButtonBox" onClick={() => setShowMenu(!showMenu)}><BsThreeDots/>
+                <button className="deletePostButtonBox" onClick={() => setShowMenu(!showMenu)}><BsThreeDots className="threeDots"/>
                     {showMenu && <button id="deletePostButton"><label>Delete Post</label></button>}
                 </button>
             </div>
