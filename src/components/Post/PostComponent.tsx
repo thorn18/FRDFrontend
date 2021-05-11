dd6afc3cf516adae8da423ddb70ecac80d03d277
Nielsen <2012791@cognizant.com>
2021-05-11 11:29:25 -0500

    css syling button box

--- src/components/Post/PostComponent.tsx
+++ src/components/Post/PostComponent.tsx
@@ -8,7 +8,7 @@ import Reply from '../../models/reply';
 import ReplyList from '../Reply/ReplyList';
 import { IconContext } from 'react-icons';
 import { BsThreeDots } from "react-icons/bs";
-import Dropdown from 'react-dropdown'
+//import Dropdown from 'react-dropdown'
 
 interface postProp {
     post: Post
@@ -33,7 +33,7 @@ function PostComponent(props: postProp) {
                 {post.user.profileImage ? <img className="pfp" src={post.user.profileImage} />
                     : <img className="pfp" src={'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'} />}
                 <span className="headerText">{post.user.username}</span>
-                <button onClick={() => setShowMenu(!showMenu)}><BsThreeDots/>
+                <button className="deletePostButtonBox" onClick={() => setShowMenu(!showMenu)}><BsThreeDots/>
                     {showMenu && <button id="deletePostButton"><label>Delete Post</label></button>}
                 </button>
             </div>
