77f61d609a22fc6b00c1e5bb2db65b9534cd2c07
Dieminger <katrina.dieminger@cognizant.com>
2021-05-11 17:16:21 -0400

    fixed up tests

--- src/views/CreatePost/CreatePost.tsx
+++ src/views/CreatePost/CreatePost.tsx
@@ -1,65 +1,57 @@
-import React, {SyntheticEvent, useState} from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import logo from '../../images/CreatePostLogo.png';
 import './CreatePost.css';
-
-import {useDispatch, useSelector} from 'react-redux';
-import {AppState} from '../../store/postReducer';
-import { useHistory } from 'react-router';
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useHistory } from 'react-router-dom';
 import PostService from '../../services/postService';
 import { NewPost } from '../../models/post';
 
-export interface PostInput {
-    description: any;
-}
+// export interface PostInput {
+//     description: any;
+// }
 
-type FormValues = {
-    image: any;
-    description: string;
-}
+// type FormValues = {
+//     image: any;
+//     description: string;
+// }
 
 function CreatePost(): JSX.Element {
-    const [input, setInput] = useState<PostInput>({description: ''});
+    const [input, setInput] = useState('');
     const [selectedFile, setSelectedFile] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false);
-    const {register, handleSubmit} = useForm<FormValues>();
     const history = useHistory();
 
     const handleInput = (e: SyntheticEvent) => {
-        let newInput = {...input};
+        let newInput = input;
 
-        if((e.target as HTMLInputElement).name === "description") {
-            newInput.description = (e.target as HTMLInputElement).value;
+        if ((e.target as HTMLInputElement).name === "description") {
+            newInput = (e.target as HTMLInputElement).value;
             setInput(newInput);
         }
     }
 
     const imageHandler = (event: any) => {
         setSelectedFile(event.target.files[0]);
-        setIsFilePicked(true);
-        console.log('filepicked?', isFilePicked);
     }
 
     const onSubmit = (event: any) => {
-        const newPost: NewPost = {username: '', image: selectedFile, description: input.description};
+        const newPost: NewPost = { username: '', image: selectedFile, description: input };
         console.log(newPost);
-        
-        //PostService.createPost(newPost);
+        PostService.createPost(newPost);
+        history.push('/home');
     }
 
-    const handleCancel = () =>{
+    const handleCancel = () => {
         history.push('/home');
     }
 
     return (
         <div id="createPost" data-testid="createPostForm">
             <img src={logo} className="createLogo" alt="Create Post Logo"></img>
-            <form>                
-                <input type="file" className="chooseImage-button" data-testid="chooseImageButton" name="file" onChange={imageHandler}/>
-                <textarea data-testid='postDescriptionInput' rows={10} cols={80} name="description" value={input.description} placeholder="Description..."/>
-            
+            <form>
+                <input type="file" className="chooseImage-button" data-testid="chooseImageButton" name="file" onChange={imageHandler} />
+                <textarea data-testid='postDescriptionInput' rows={10} cols={80} name="description" value={input} onChange={handleInput} placeholder="Description..." />
+
                 <div className='actionButtonContainer'>
-                    <button className="buttonCancel" data-testid='cancelButton'  onClick={handleCancel}>Cancel</button>
+                    <button className="buttonCancel" data-testid='cancelButton' onClick={handleCancel}>Cancel</button>
                     <button type='button' className="buttonCreatePost" data-testid='createPostButton' onClick={onSubmit}>Create Post</button>
                 </div>
             </form>
--- test/CreatePostValidation.test.tsx
+++ test/CreatePostValidation.test.tsx
@@ -1,27 +1,106 @@
 import React, { useState } from "react";
 import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import "@testing-library/jest-dom/extend-expect";
 import { useForm } from 'react-hook-form';
-import CreatePost from '../src/views/CreatePost/CreatePost';
 import { screen, render, fireEvent, cleanup } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
+import PostService from '../src/services/postService';
+import { Router, useHistory } from "react-router-dom";
+import { createMemoryHistory } from 'history';
+import CreatePost from "../src/views/CreatePost/CreatePost";
+import thunk from "redux-thunk";
+
+const mockPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        push: mockPush,
+    }),
+}));
+
+jest.mock('react-hook-form', () => ({
+    ...jest.requireActual('react-hook-form'),
+    useForm: jest.fn(),
+}));
 
 jest.mock('react', () => ({
     ...jest.requireActual('react'),
     useState: jest.fn()
 }));
 
-jest.mock('react-hook-form', () => ({
-    ...jest.requireActual('react-hook-form'),
-    useForm: jest.fn()
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useDispatch: jest.fn(),
+    useSelector: jest.fn()
 }));
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useHistory: jest.fn()
-}));
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+let store;
 
-beforeEach(() => {
-    jest.clearAllMocks();
+
+afterEach(cleanup);
+
+describe('Tests for Form Input', () => {
+
+    let input = '';
+    let selectedFile = undefined;
+
+    const setupWithSetState = () => {
+        (useState as jest.Mock).mockImplementationOnce(() => [input, jest.fn().mockImplementation((x) => {
+            let newInput = input;
+            newInput = x;
+            input = newInput;
+        })]).mockImplementationOnce(() => [selectedFile, jest.fn().mockImplementationOnce((x) => {
+            let newFile = selectedFile;
+            newFile = x;
+            selectedFile = newFile;
+        })])
+    }
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        store = mockStore({
+            postsState: {
+                posts: [],
+                loading: false,
+                hasMoreItems: true
+            },
+            userState: {
+                token: '',
+                loggedIn: false,
+                username: ''
+            }
+        });
+    })
+
+    it('Test that clicking create post button, the form submits', () => {
+        setupWithSetState();
+        const { getByTestId } = render(<Provider store={store}><CreatePost /></Provider>)
+        fireEvent.click(getByTestId('createPostButton'));
+        expect(mockPush).toHaveBeenCalledWith('/home');
+    })
+
+    it('Test that onSubmit dispatches to PostService', () => {
+        let dispatch = jest.fn();
+        dispatch.mockImplementation((x): void => {
+            return;
+        });
+        (useDispatch as jest.Mock).mockImplementation(() => {
+            return dispatch;
+        })
+
+        setupWithSetState();
+
+        PostService.createPost = jest.fn().mockResolvedValue(200);
+
+        const { getByTestId } = render(
+            <Provider store={store}>
+                <CreatePost />
+            </Provider>);
+
+        fireEvent.click(getByTestId('createPostButton'));
+        expect(PostService.createPost).toHaveBeenCalledTimes(1);
+    })
 })
