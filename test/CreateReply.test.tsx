03ed1c5527585afe1cdd87b6660a98060aa583fa
Jonathan Dawson <jonathan.dawson@cognizant.com>
2021-05-13 16:49:26 -0500

    wip test for input

--- test/CreateReply.test.tsx
+++ test/CreateReply.test.tsx
@@ -2,42 +2,77 @@ import React, { useState } from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import store from '../src/store/store';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+
 import CreateReplyComponent from '../src/components/Reply/CreateReply'
 import { post0, noProfilePic } from './testData';
 import Post from '../src/models/post'
+import * as fivePosts from './fivePosts.json';
 
-describe('Tests for Create Reply Component that', () => {
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useState: jest.fn()
+}));
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react'),
+    useDispatch: jest.fn(),
+    useSelector: jest.fn()
+}));
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+let store;
+
+beforeEach(() => {
+    store = mockStore({
+        postsState: {
+            posts: fivePosts.items,
+            loading: false,
+            hasMoreItems: true
+        },
+        userState: {
+            username: 'Bob',
+            token: 'aToken',
+            loggedIn: true,
+            error: undefined
+          }
+    });
+  });
+
+describe('Tests for Create Reply Component, when logged in, that', () => {
 
     it('will make sure that Create Reply Component is visible', () => {
-        const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
+        const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0} /> </Provider>);
         expect(getByTestId('createReply')).toBeVisible();
     });
 
-    describe('Input box tests that ', () => {
-        // let input = 'test';
-        // (useState as jest.Mock).mockImplementationOnce(() => [input, jest.fn().mockImplementation((x) => {
-        //     let newinput = input;
+    it.skip('Test to make sure that button is visible', () => {
+        const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0} /> </Provider>);
+        expect(getByTestId('createReplyButton')).toBeVisible();
+    });
 
-        //     input = newinput;
-        // })])
+    describe.skip('Input box tests that ', () => {
+        let input = 'test';
 
         it('will make sure that input is visible', () => {
-            const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
+            const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0} /> </Provider>);
             expect(getByTestId('createReplyInput')).toBeVisible();
         });
 
         it('can handle input', () => {
-            const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
+            let testInput = 'wow. such great photo. many like.';
+            (useState as jest.Mock).mockImplementationOnce(() => [input, jest.fn().mockImplementation((x) => {
+                input = testInput;
+            })])
+
+            const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0} /> </Provider>);
             let content = getByTestId('createReplyInput');
-            let testInput = 'wow. such great photo. many like.'
+            
             fireEvent.change(content, { target: { value: testInput } });
             //expect(input).toEqual(testInput);
         })
     })
 
-    it('Test to make sure that button is visible', () => {
-        const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
-        expect(getByTestId('createReplyButton')).toBeVisible();
-    });
+
 })
