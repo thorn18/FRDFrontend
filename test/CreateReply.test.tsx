a2e34f4251c47735e0e0c92374debcb56b328f67
Jonathan Dawson <jonathan.dawson@cognizant.com>
2021-05-13 16:23:42 -0500

    test input

--- test/CreateReply.test.tsx
+++ test/CreateReply.test.tsx
@@ -7,17 +7,34 @@ import CreateReplyComponent from '../src/components/Reply/CreateReply'
 import { post0, noProfilePic } from './testData';
 import Post from '../src/models/post'
 
-describe('Tests for Create Reply Component', () => {
+describe('Tests for Create Reply Component that', () => {
 
-    it('Test to make sure that Create Reply Component is visible', () => {
+    it('will make sure that Create Reply Component is visible', () => {
         const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
         expect(getByTestId('createReply')).toBeVisible();
     });
 
-    it('Test to make sure that input is visible', () => {
-        const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
-        expect(getByTestId('createReplyInput')).toBeVisible();
-    });
+    describe('Input box tests that ', () => {
+        // let input = 'test';
+        // (useState as jest.Mock).mockImplementationOnce(() => [input, jest.fn().mockImplementation((x) => {
+        //     let newinput = input;
+
+        //     input = newinput;
+        // })])
+
+        it('will make sure that input is visible', () => {
+            const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
+            expect(getByTestId('createReplyInput')).toBeVisible();
+        });
+
+        it('can handle input', () => {
+            const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
+            let content = getByTestId('createReplyInput');
+            let testInput = 'wow. such great photo. many like.'
+            fireEvent.change(content, { target: { value: testInput } });
+            //expect(input).toEqual(testInput);
+        })
+    })
 
     it('Test to make sure that button is visible', () => {
         const { getByTestId } = render(<Provider store={store}> <CreateReplyComponent post={post0}/> </Provider>);
