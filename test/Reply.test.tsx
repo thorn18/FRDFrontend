3205a8e617eaee0c0fb5fd7b02d6303ae78bd677
Jonathan Dawson <jonathan.dawson@cognizant.com>
2021-06-11 10:43:02 -0500

    fix reply test

--- test/Reply.test.tsx
+++ test/Reply.test.tsx
@@ -3,20 +3,36 @@ import { render, cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 import ReplyComponent from '../src/components/Reply/Reply'
 import Reply from '../src/models/reply';
+import { Provider, useSelector } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useSelector: jest.fn()
+}));
+
+const testComment: Reply = {
+    id: 'testId',
+    username: 'testUser',
+    content: 'bla bla bla',
+    timestamp: new Date(),
+    postId: 'testPostId'
+};
+
+(useSelector as jest.Mock).mockImplementationOnce(() => true)
+    .mockImplementationOnce(() => 'aToken')
+    .mockImplementationOnce(() => testComment);
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares)
+let store = mockStore({});
 
 afterEach(cleanup);
 
 describe('Tests for comment component', () => {
     test('that you can see the commenter\'s username', () => {
-        const testComment: Reply = {
-            id: 'testId',
-            username: 'testUser',
-            content: 'bla bla bla',
-            timestamp: new Date(),
-            postId: 'testPostId'
-        }
-
-        const { getByTestId } = render(<ReplyComponent reply={testComment}/>);
+        const { getByTestId } = render(<Provider store={store}> <ReplyComponent reply={testComment} /> </Provider>);
 
         expect(getByTestId('commenter')).toBeVisible();
         expect(getByTestId('comment-content')).toBeVisible();
