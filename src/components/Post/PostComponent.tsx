d26228a232dfffc5c0a4f69df61e3de492573564
Jonathan Dawson <jonathan.dawson@cognizant.com>
2021-06-24 08:24:06 -0500

    fix spacing in post component

--- src/components/Post/PostComponent.tsx
+++ src/components/Post/PostComponent.tsx
@@ -14,13 +14,13 @@ import CreateReplyComponent from '../Reply/CreateReply';
 interface postProp {
     post: Post
 }
- 
+
 /**
  * This component displays the individual post content
  * @param: post - the post consist of the user, post image and description, and replies
  */
 function PostComponent(props: postProp) {
-    
+
     const { post } = props;
     const [showMenu, setShowMenu] = useState(false);
     const dispatch = useDispatch();
@@ -28,27 +28,27 @@ function PostComponent(props: postProp) {
     const loggedin: boolean = useSelector((state: AppState) => state.userState.loggedIn);
     const token: string | null = useSelector((state: AppState) => state.userState.token);
     const posts = useSelector((state: AppState) => state.postsState)
-    useEffect(()=>{}, [posts])
+    useEffect(() => { }, [posts])
 
     function deletePost() {
-        if (loggedin && token)  dispatch(postService.deletePost(post.post.id, token, false));
+        if (loggedin && token) dispatch(postService.deletePost(post.post.id, token, false));
     }
 
     return (
         <div className="postCard" data-testid='post-card'>
             <div className="postHeader">
                 {post.user.profileImage ? <img className="pfp" src={post.user.profileImage} alt={`${post.user.username}'s Profile`} />
-                    : <img className="pfp" src={'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'} alt={`${post.user.username}'s Profile`}/>}
+                    : <img className="pfp" src={'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'} alt={`${post.user.username}'s Profile`} />}
                 <span className="headerText">{post.user.username}</span>
-                {loggedin && user === post.user.username && <button className="deletePostButtonBox" data-testid = "deleteButtonBox" onClick={() => setShowMenu(!showMenu)}><BsThreeDots className="threeDots"/>
-                    {showMenu && <button id="deletePostButton" data-testid = "deleteButton" onClick={deletePost}><label>Delete Post</label></button>}
+                {loggedin && user === post.user.username && <button className="deletePostButtonBox" data-testid="deleteButtonBox" onClick={() => setShowMenu(!showMenu)}><BsThreeDots className="threeDots" />
+                    {showMenu && <button id="deletePostButton" data-testid="deleteButton" onClick={deletePost}><label>Delete Post</label></button>}
                 </button>}
             </div>
             <div className="imageDiv">
-                <img className="postImage" src={'http://35.223.52.208/api/image/' + post.post.imageId} alt='Post Content'/>
+                <img className="postImage" src={'http://35.223.52.208/api/image/' + post.post.imageId} alt='Post Content' />
             </div>
             <div className="postStats">
-                <img src={likes} className="likesIcon" alt='Likes'/>
+                <img src={likes} className="likesIcon" alt='Likes' />
                 <div className="postMeta">
                     {post.post.likes} likes &nbsp; {post.comments.totalCount} {post.comments.totalCount === 1 ? 'comment' : 'comments'}
                 </div>
@@ -57,11 +57,11 @@ function PostComponent(props: postProp) {
                 <p className="descriptionUser">{post.post.username} <span className="postDesc">{post.post.description}</span></p>
             </div>
             <div className="replyList">
-                <ReplyList post={props.post}/>
+                <ReplyList post={props.post} />
                 {(post.comments.items).map((reply: Reply) => {
-                    return <ReplyComponent reply={reply} key={reply.id}/>
+                    return <ReplyComponent reply={reply} key={reply.id} />
                 })}
-                {loggedin && <CreateReplyComponent post={props.post}/>}
+                {loggedin && <CreateReplyComponent post={props.post} />}
             </div>
         </div>
     );
